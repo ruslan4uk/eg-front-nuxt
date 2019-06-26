d58521549ccdf77824b17c1aa52fdbe8ce2ec020
Ruslan <rusel91@idz.ru>
2019-06-26 17:27:25 +0400

    fix

---
+++ pages/auth/confirm.vue
@@ -0,0 +1,26 @@
+<template>
+    <section class="auth-confirm footer-fix">
+        <b-container>
+            <b-row class="align-items-center">
+                <b-col cols="12" lg="6" class="text-center mb-5">
+                    <img src="~assets/images/auth/confirm.png" alt="">
+                </b-col>
+                <b-col cols="12" lg="6" class="mb-5">
+                    <div class="auth__title mb-2">Осталось еще чуть-чуть</div>
+                    <div class="auth__subtitle mb-3">Чтобы завершить регистрацию подтвердите свой эл.адрес</div>
+                    <a href="" class="btn btn-blue">Обновить</a>
+                </b-col>
+            </b-row>
+        </b-container>
+    </section>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style scoped>
+
+</style>
