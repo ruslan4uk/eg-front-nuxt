afcf47b66d86566c09784921161eb7a385142975
Ruslan Safin <rusel91@idz.ru>
2019-07-30 17:17:52 +0400

    add google meta

--- nuxt.config.js
+++ nuxt.config.js
@@ -18,7 +18,8 @@ export default {
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: pkg.description },
-      { name: 'yandex-verification', content: '1b17652caa4578f5' }
+      { name: 'yandex-verification', content: '1b17652caa4578f5' },
+      { name: 'google-site-verification', content: 'Jrlf7j1xzEEYlhh8m_Og-nz5Q2Q2zJ_Wch1oqOJCIk4' }
     ],
     link: [
       { rel: 'icon', type: 'image/png', href: '/fav_64x64.png' },
