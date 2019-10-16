8c1a1e5ffbc083e39f4d911cc9fac92f0a1d2217
Ruslan <rusel91@idz.ru>
2019-10-17 00:11:58 +0400

    docker letsencrypt

--- plugins/laravelEcho.js
+++ plugins/laravelEcho.js
@@ -7,12 +7,11 @@ window.io = require('socket.io-client');
 
 window.Echo = new Echo({
   broadcaster: 'socket.io',
-  host: window.location.hostname + ':6001',
+  host: 'ws.' + window.location.hostname + ':6001',
   auth: {headers: {Authorization: token}}
 });
 
 export default function({ $axios, store, app, redirect, query }) {
-  // console.log(store.state.auth.user)
   window.Echo.private(`messenger.${store.state.auth.user.id}`)
     .listen('Messenger.DialogsEvent', ({message}) => {
       store.commit('auth/UPDATE_UNREAD', 1);
