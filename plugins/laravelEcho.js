325d90bab962be283ec7600d53a34da17045a6a3
Ruslan <rusel91@idz.ru>
2019-10-17 14:35:12 +0400

    docker letsencrypt

--- plugins/laravelEcho.js
+++ plugins/laravelEcho.js
@@ -7,7 +7,7 @@ window.io = require('socket.io-client');
 
 window.Echo = new Echo({
   broadcaster: 'socket.io',
-  host: window.location.hostname + ':6001',
+  host: 'ws.' + window.location.hostname + ':6001',
   auth: {headers: {Authorization: token}}
 });
 
